f94d964becd7226ae53af0abe8f5f321c67dca85
russiantech <chrisjsme@gmail.com>
2024-05-31 02:27:52 +0000

    task-01 fixed

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """
-Module that defines a function to obfuscate PII data in a log message.
+Module that defines a function to obfuscate PII data in a log message and
+a RedactingFormatter class to format log messages.
 """
 
 import re
+import logging
 from typing import List
 
 
@@ -26,3 +28,30 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     pattern = f"({'|'.join(fields)})=.*?{separator}"
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}{separator}",
                   message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Formats the log record, redacting sensitive information.
+
+        Args:
+            record (logging.LogRecord): The log record to be formatted.
+
+        Returns:
+            str: The formatted log record with sensitive information redacted.
+        """
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.msg, self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
