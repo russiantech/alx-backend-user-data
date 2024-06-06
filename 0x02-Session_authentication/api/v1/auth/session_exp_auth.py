7312afc6ab8b236b1a80026d2316a5da8eaf0652
russiantech <chrisjsme@gmail.com>
2024-06-06 15:22:05 +0000

    fixed blank line, non-compliance for task 9

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -5,9 +5,10 @@ import datetime
 from api.v1.auth.session_auth import SessionAuth
 from os import getenv
 
+
 class SessionExpAuth(SessionAuth):
     """Session Expiration Authentication"""
-    
+
     def __init__(self):
         """Initialize the SessionExpAuth class"""
         super().__init__()
@@ -21,7 +22,7 @@ class SessionExpAuth(SessionAuth):
         session_id = super().create_session(user_id)
         if not session_id:
             return None
-        
+
         session_dict = {
             "user_id": user_id,
             "created_at": datetime.datetime.now()
@@ -33,19 +34,20 @@ class SessionExpAuth(SessionAuth):
         """Get user ID from session ID considering session expiration"""
         if session_id is None:
             return None
-        
+
         session_dict = self.user_id_by_session_id.get(session_id)
         if not session_dict:
             return None
-        
+
         if self.session_duration <= 0:
             return session_dict.get("user_id")
-        
+
         created_at = session_dict.get("created_at")
         if not created_at:
             return None
-        
-        if created_at + datetime.timedelta(seconds=self.session_duration) < datetime.datetime.now():
+
+        if created_at + datetime.timedelta(
+                seconds=self.session_duration) < datetime.datetime.now():
             return None
-        
+
         return session_dict.get("user_id")
