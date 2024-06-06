7a45c0eb3adeaee309aa817fc26b8b7a5326e21e
russiantech <chrisjsme@gmail.com>
2024-06-06 14:03:06 +0000

    blank line issues fixed for task-8

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -39,3 +39,15 @@ def auth_session_login():
     response.set_cookie(session_name, session_id)
 
     return response
+
+
+@app_views.route(
+        '/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+def auth_session_logout():
+    """DELETE /api/v1/auth_session/logout"""
+    from api.v1.app import auth  # Avoiding circular import
+
+    if not auth.destroy_session(request):
+        abort(404)
+
+    return jsonify({}), 200
