9a202acbb3c27ece2fe9f6634269fb12aab8584a
russiantech <chrisjsme@gmail.com>
2024-06-06 07:56:23 +0000

    base64 issues fixed

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """
 Module for BasicAuth class
 """
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -9,18 +10,6 @@ class BasicAuth(Auth):
     """
     BasicAuth class for handling Basic Authentication
     """
-    def extract_base64_authorization_header(
-            self, authorization_header: str) -> str:
-        """
-        Extracts the Base64 part of the Authorization header
-        """
-        if authorization_header is None:
-            return None
-        if not isinstance(authorization_header, str):
-            return None
-        if not authorization_header.startswith("Basic "):
-            return None
-        return authorization_header[6:]
 
     def extract_base64_authorization_header(
             self, authorization_header: str) -> str:
