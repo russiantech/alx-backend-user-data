6ae275b3f765e179290ba001d708f8bbd1bd9871
russiantech <chrisjsme@gmail.com>
2024-06-06 12:07:22 +0000

    session based authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -21,6 +21,9 @@ if AUTH_TYPE == 'basic_auth':
 elif AUTH_TYPE == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
+if AUTH_TYPE == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 else:
     from api.v1.auth.auth import Auth
     auth = Auth()
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""api/v1/auth/session_auth.py"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """Session authentication"""
+
+    pass
